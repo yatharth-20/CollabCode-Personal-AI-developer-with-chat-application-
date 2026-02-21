b322e80bbaa2cf9bf15713256741359215adcd51
Nikhil <nikhilanand0904@gmail.com>
2026-02-21 14:20:45 +0530

    Added user side panel

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate,  useLocation } from 'react-router-dom'
 
 const Project = () => {
   
   const location = useLocation();
+
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
 
   console.log(location.state);
   
@@ -18,6 +20,7 @@ const Project = () => {
         >
 
           <button
+            onClick={() => setIsSidePanelOpen(!isSidePanelOpen)}
             className='p-2'
           >
 
@@ -60,18 +63,49 @@ const Project = () => {
             <div className="inputField w-full flex">
 
               <input
-              className="p-2 px-20 rounded-md bg-white border-none outline-none" 
+              className="p-2 px-20 rounded-md bg-white border-none outline-none grow" 
               type="text" placeholder='Enter Message' />
 
               <button
-              className='grow'
+              className='px-3'
               ><i className='ri-send-plane-fill p-2 rounded-2xl cursor-pointer bg-green-400'></i></button>
 
             </div>
           
         </div>
 
-        <div className="sidePanel w-36 h-60 absolute -left-full top-0 bg-green-400"></div>
+        <div className={`sidePanel w-full h-full flex flex-col gap-2 absolute transition-all ${isSidePanelOpen ? 'translate-x-0' : '-translate-x-full'} top-0 bg-slate-50`}>
+
+          <header className='flex justify-end p-2 px-4 bg-slate-300'>
+
+            <button
+              onClick={() => setIsSidePanelOpen(false)}
+              className='p-2'
+            >
+              <i class=" cursor-pointer ri-close-large-line"></i>
+            </button>
+            
+          </header>
+
+          <div className="users flex flex-col gap-2 p-2">
+
+            <div className="user cursor-pointer hover:bg-slate-200 p-2 flex gap-2 items-center">
+
+              <div
+              className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'
+              >
+                <i className="ri-user-fill absolute"></i>
+              </div>
+
+              <h1 
+              className='font-semibold text-lg'
+              >User</h1>
+              
+            </div>
+            
+          </div>
+          
+        </div>
         
       </section>
 
