735af2968f79428955c04d800b5e72c8b130b16d
Nikhil <nikhilanand0904@gmail.com>
2026-02-21 13:38:13 +0530

    Added Messaging UI

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -11,7 +11,7 @@ const Project = () => {
    <main
     className='h-screen w-screen flex '
    >
-      <section className="left h-full min-w-100 bg-red-300">
+      <section className="left relative flex flex-col h-full min-w-100 bg-slate-300">
 
         <header
           className="flex justify-end p-2 px-4 w-full bg-slate-200"
@@ -27,6 +27,51 @@ const Project = () => {
 
         </header>
         
+        <div className="conversation-area grow flex flex-col">
+
+          <div className="message-box grow flex flex-col p-2 gap-3">
+
+            {/* Incoming Message */}
+
+            <div className="message max-w-56 flex flex-col gap-1 p-2 bg-slate-50 w-fit rounded-md">
+
+              <small
+              className='opacity-60 text-xs'
+              >example@gmail.com</small>
+
+              <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
+
+            </div>
+
+            {/* Your Message */}
+
+            <div className="ml-auto max-w-56 message flex flex-col gap-1 p-2 bg-slate-50 w-fit rounded-md">
+
+              <small
+              className='opacity-60 text-xs'
+              >example@gmail.com</small>
+
+              <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
+
+            </div>
+
+          </div>
+
+            <div className="inputField w-full flex">
+
+              <input
+              className="p-2 px-20 rounded-md bg-white border-none outline-none" 
+              type="text" placeholder='Enter Message' />
+
+              <button
+              className='grow'
+              ><i className='ri-send-plane-fill p-2 rounded-2xl cursor-pointer bg-green-400'></i></button>
+
+            </div>
+          
+        </div>
+
+        <div className="sidePanel w-36 h-60 absolute -left-full top-0 bg-green-400"></div>
         
       </section>
 
