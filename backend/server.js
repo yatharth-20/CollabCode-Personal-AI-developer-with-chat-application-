331c78ed2b0fab20b19079f1ce97e9eb115f779c
Nikhil <nikhilanand0904@gmail.com>
2026-03-10 11:43:42 +0530

    Server setup

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
 import dotenv from 'dotenv';
+import http from 'http';
+import app from './app.js';
+import { Server } from 'socket.io';
+
+
 dotenv.config();    // to use environmental variables
 
 console.log("✅ ENV LOADED:", process.env.MONGODB_URI);
-
-import http from 'http';
-import app from './app.js';
 
 const port = process.env.PORT || 3000;
 
 
 const server = http.createServer(app);
+const io = new Server(server);
+
+
+io.on('connection', socket => {
+
+    console.log('A user connected');
+
+    socket.on('event', data => { /* … */ });
+    socket.on('disconnect', () => { /* … */ });
+});
+
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
