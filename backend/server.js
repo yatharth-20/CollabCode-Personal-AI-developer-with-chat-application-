ee7839bddd7b2b2af8726073f3c4eb2ac394ac5d
Nikhil <nikhilanand0904@gmail.com>
2026-03-13 23:18:11 +0530

    Authentication through Socket.io

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import http from 'http';
 import app from './app.js';
 import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';
 
 
 dotenv.config();    // to use environmental variables
@@ -13,6 +14,29 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+
+io.use((socket, next) => {
+    try {
+
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[ 1 ];
+        if (!token) {
+            return next(new Error('Authentication error'));
+        }
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decode) {
+            return next(new Error('Authentication error'));
+        }
+
+        socket.user = decode;
+        next();
+        
+    } catch (error) {
+        next(error); // next error ke sath call hota hai to socket connect nahi hota
+    }
+})
 
 
 io.on('connection', socket => {
