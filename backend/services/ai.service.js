9bc73d319c125e97b0c25b50ca4e571589d3664c
Nikhil Anand <nikhilanand0904@gmail.com>
2026-04-07 23:20:02 +0530

    Refactor ai-service

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -4,8 +4,8 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({
     model: "gemini-2.5-flash",
     generationConfig: {
-        responseMimeType : "application/json",
-        
+        responseMimeType: "application/json",
+
     },
     systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development. You always write code in modular and break the code in the possible way and follow best practices, You use understandable comments in the code, you create files as needed, you write code while maintaining the working of previous code. You always follow the best practices of the development You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.You don't need to explain what you do or what your role is. All you need to do is to write code and nothing else. You always write code in the best possible way.
 
@@ -51,16 +51,16 @@ const model = genAI.getGenerativeModel({
                             }
                         }
                     ",
+                },
 
-                    "buildCommand": {
-                        mainItem: "npm",
-                        commands: ]["install"]
-                    },
+                "buildCommand": {
+                    mainItem: "npm",
+                    commands: ]["install"]
+                },
 
-                    "startCommand": {
-                        mainItem: "node",
-                        commands: ["app.js"]
-                    }
+                "startCommand": {
+                    mainItem: "node",
+                    commands: ["app.js"]
                 }
             }
         }
