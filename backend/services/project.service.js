387563a217adab1e833f112874b13b5691a2d62a
Nikhil Anand <nikhilanand0904@gmail.com>
2026-04-16 16:02:36 +0530

    Add updateFileTree function for project file updates

--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -95,3 +95,24 @@ export const getProjectById = async ({projectId}) => {
 
     return project;
 }
+
+export const updateFileTree = async ({ projectId, fileTree }) => {
+    if (!projectId) 
+        throw new Error("projectId is required")
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) 
+        throw new Error("Invalid projectId")
+
+    if (!fileTree) 
+        throw new Error("fileTree is required")
+
+    const project = await projectModel.findOneAndUpdate({
+        _id: projectId
+    }, {
+        fileTree
+    }, {
+        new: true
+    })
+
+    return project;
+}
