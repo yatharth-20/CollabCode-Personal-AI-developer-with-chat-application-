907bfadc207681fa70f56974f3868a422af052bc
Nikhil Anand <nikhilanand0904@gmail.com>
2026-04-16 16:00:38 +0530

    Add route to update file tree for a project

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -29,4 +29,11 @@ router.get('/get-project/:projectId',
     projectController.getProjectById
 )
 
+router.put('/update-file-tree',
+    authMiddleware.authUser,
+    body('projectId').isString().withMessage('Project ID is required'),
+    body('fileTree').isObject().withMessage('File tree is required'),
+    projectController.updateFileTree
+)
+
 export default router;
